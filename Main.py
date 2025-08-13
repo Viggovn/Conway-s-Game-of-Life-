75d5c2ebcca84b724f684bc84e07fe4df9bd7b8d
Viggo van Nuil <viggovannuil@gmail.com>
2025-08-13 22:44:47 +0200

    random living cell generation

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import random
+
 UserInputSizeGridX = int(input("How Big do you want the X axis to be? "))
 UserInputSizeGridY = int(input("How Big do you want the Y axis to be? "))
 TotalCells = UserInputSizeGridX * UserInputSizeGridY
@@ -6,8 +8,16 @@ Board = []
 
 for i in range(TotalCells):
     Board.append(0)
-    
+
+for i in range(len(Board)):
+    AliveOrDeath = random.randint(1, 10)
+    if AliveOrDeath > 7:
+        Board[i] = 1
+    else:
+        continue
+ 
 for i, Cells in enumerate(Board, start=1):
     print(Cells, end=" ")
     if i % UserInputSizeGridX == 0:
         print()
+
